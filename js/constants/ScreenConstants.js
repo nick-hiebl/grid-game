ea38839d123e30c3383b5f64afd2eff388e63bcd
nick-hiebl <nicholas.hiebl@gmail.com>
2022-08-20 00:51:30 +1000

    Use more visually pleasing canvas scale

--- js/constants/ScreenConstants.js
+++ js/constants/ScreenConstants.js
@@ -1,5 +1,5 @@
-export const CANVAS_WIDTH = 320;
-export const CANVAS_HEIGHT = 180;
+export const CANVAS_WIDTH = 1280;
+export const CANVAS_HEIGHT = 720;
 
 export const HORIZONTAL_TILES = 32;
 export const VERTICAL_TILES = 18;
@@ -8,8 +8,8 @@ export const PIXELS_PER_TILE = 10;
 
 export const PIXEL_WIDTH = CANVAS_WIDTH / HORIZONTAL_TILES / PIXELS_PER_TILE;
 
-export const ON_SCREEN_CANVAS_WIDTH = 4 * CANVAS_WIDTH;
-export const ON_SCREEN_CANVAS_HEIGHT = 4 * CANVAS_HEIGHT;
+export const ON_SCREEN_CANVAS_WIDTH = 1280;
+export const ON_SCREEN_CANVAS_HEIGHT = 720;
 
 export const UI_CANVAS_WIDTH = ON_SCREEN_CANVAS_WIDTH;
 export const UI_CANVAS_HEIGHT = ON_SCREEN_CANVAS_HEIGHT;
